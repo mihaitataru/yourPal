232b6ede9c66b1b07cc796c1239194f4472b0fa1
Mihai Tataru <mihai.tataru2002@gmail.com>
2022-11-19 14:30:19 +0200

    initialization

---
+++ src/test/java/com/fourEngineers/yourPal/YourPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourEngineers.yourPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourPalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
