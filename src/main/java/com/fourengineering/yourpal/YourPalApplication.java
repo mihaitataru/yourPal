4c947eee28d0a13f96f590dcadcc715918562fc8
Mihai Tataru <mihai.tataru2002@gmail.com>
2022-11-19 18:59:58 +0200

    init

---
+++ src/main/java/com/fourengineering/yourpal/YourPalApplication.java
@@ -0,0 +1,13 @@
+package com.fourengineering.yourpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourPalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fourengineering/yourpal/YourPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourengineering.yourpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourPalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
