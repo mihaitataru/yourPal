c0eea3158a5fe726d20d745624ad6196e3dd4dec
Cezar <cezar.naghi@gmail.com>
2022-11-19 05:11:25 -0800

    yoooooo

---
+++ src/main/java/com/fourengineering/yourpal2/YourPal2Application.java
@@ -0,0 +1,13 @@
+package com.fourengineering.yourpal2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourPal2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourPal2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fourengineering/yourpal2/YourPal2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourengineering.yourpal2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourPal2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
